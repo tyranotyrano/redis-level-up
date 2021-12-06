9081981470fbb01083a93ac772893d69a068832e
tyanotyrano <tyrano09@gmail.com>
2021-12-06 23:33:18 +0900

    Initial commit

---
+++ src/main/java/com/tyranotyrano/RedisLevelUpApplication.java
@@ -0,0 +1,13 @@
+package com.tyranotyrano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLevelUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLevelUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tyranotyrano/RedisLevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyranotyrano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLevelUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
