7e987ca9fe4cb28c3ec7b758f0993436b2428f3a
tyanotyrano <tyrano09@gmail.com>
2021-12-30 23:26:00 +0900

    feat(ObjectRedisService.java): Redis List Operation 기능 구현
    Redis List Operation 기능을 구현한다.
    - ObjectRedisController : Redis List Operation API 작성
    - ObjectRedisService : Redis List Operation 기능 구현

--- src/main/java/com/tyranotyrano/service/ObjectRedisService.java
+++ src/main/java/com/tyranotyrano/service/ObjectRedisService.java
@@ -1,5 +1,8 @@
 package com.tyranotyrano.service;
 
+import java.util.List;
+
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -22,5 +25,15 @@ public class ObjectRedisService {
     public void create(CreateCacheRq rq) {
         ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
         valueOperations.set(rq.getKey(), rq.getValue());
+    }
+
+    public void add(CreateCacheRq rq) {
+        ListOperations<String, Object> listOperations = redisTemplate.opsForList();
+        listOperations.leftPush(rq.getKey(), rq.getValue());
+    }
+
+    public List<Object> findAll(String key) {
+        ListOperations<String, Object> listOperations = redisTemplate.opsForList();
+        return listOperations.range(key, 0, listOperations.size(key));
     }
 }
